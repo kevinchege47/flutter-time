5fd06fba7448455dc75242c97b8dbc731f1bf6ef
kevinchege47 <chegekevin001@gmail.com>
2023-02-24 21:52:30 +0300

    #7 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-        home: Scaffold(
+void main() => runApp(
+      const MaterialApp(
+        home: Home()
+      )
+    );
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text("My app bar"),
         centerTitle: true,
+        backgroundColor: Colors.green[600],
       ),
       body: const Center(
-        child: Text("Hello Ninjas"),
+        child: Text(
+          "Hello Ninjas",
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.black54,
+              letterSpacing: 2.0,
+              fontFamily: 'Tilt'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
+        backgroundColor: Colors.green[600],
         child: const Text("Click"),
       ),
       // ),
-    )));
+    );
+  }
+}
