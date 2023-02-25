511ac12f572b2e6a766535986c894e14ef728db6
kevinchege47 <chegekevin001@gmail.com>
2023-02-26 01:07:34 +0300

    #9 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        home: Home()
-      )
-    );
+void main() => runApp(const MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -18,14 +15,43 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green[600],
       ),
       body: Center(
-        child: Image.network('https://images.unsplash.com/photo-1676840642714-2ebccd286ca2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8aVVJc25WdGpCMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60')
+        child: IconButton(
+          onPressed: () {
+            if (kDebugMode) {
+              print('pressed me');
+            }
+          },
+          icon: const Icon(Icons.access_alarm),
+        ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.green[600],
-        child: const Text("Click"),
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        verticalDirection: VerticalDirection.up,
+        children: const [
+          Text(
+            'Flutter',
+            style: TextStyle(
+                color: Colors.yellow,
+                fontSize: 30.0
+            ),
+          ),
+         FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.green[600],
+          child: Text("Click"),
+        ),
       ),
-      // ),
+          Text(
+            'Flutter',
+            style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20.0
+            ),
+          ),
+        ],
+      ),
+
     );
   }
 }
